730472b162eb64d256a87f1ee71a20e7128fd5e2
Aghiad Khertabeel <aghydkartbeel@gmail.com>
2025-07-15 14:44:39 +0200

    Rename files for improved module structure and organization
    Signed-off-by: Aghiad Khertabeel <aghydkartbeel@gmail.com>

--- additiv/additive_cipher.py
+++ additiv/additive_cipher.py
@@ -19,7 +19,6 @@ import argparse
 import sys
 import os
 
-# Add parent directory to Python path to import util module
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from util.file_util import read_file, write_file
